df4e9815c938b9fc0b8f5ffccc02741d9436876b
AlleksDev <aimr2910@gmail.com>
2026-02-02 19:56:56 -0600

    Evaluacion de aptitud

--- main.py
+++ main.py
@@ -8,21 +8,41 @@ def generar_individuo(longitud, valores_posibles):
     random.shuffle(individuo)
     return individuo
 
-def generar_poblacion(num_individuos, longitud_cromosoma):
+def generar_poblacion(num_individuos, longitud_cromosoma, valores_posibles):
     poblacion = []
     for ind in range(num_individuos):
-        individuo = generar_individuo(longitud_cromosoma, valores_posibles=[0, 1, 5])
+        individuo = generar_individuo(longitud_cromosoma, valores_posibles)
         poblacion.append(individuo)
     
     return poblacion
 
+def evaluar_individuo(individuo, vals_posiciones, valores_posibles):
+    total = 0
+    for idx, gene in enumerate(individuo):
+        if gene is not None:
+            peso = vals_posiciones.get(idx, 0)
+            masa = valores_posibles.get(gene, 0)
+            print(f"Posición: {idx}, Gene: {gene}, Masa: {masa}, Peso: {peso}")
+            total += masa * peso
+    return abs(total)
+
 def main():
     num_individuos = 10
-    longitud_cromosoma = 5
-    valores_posibles = [0, 1, 2, 3, 4, 5]
+    valores_posibles = {
+        1: 12.0,
+        2: 15.5,
+        3: 20.3,
+        4: 7.8,
+    }
+    vals_posiciones = {
+        0: -1,
+        1: 0,
+        2: 1,
+    }
     
-    poblacion = generar_poblacion(num_individuos, longitud_cromosoma)
-    
+    poblacion = generar_poblacion(num_individuos, len(vals_posiciones), list(valores_posibles.keys()))
+    print(poblacion[0])
+    print("Aptitud", evaluar_individuo(poblacion[0], vals_posiciones, valores_posibles))
     for idx, individuo in enumerate(poblacion):
         print(f"Individuo {idx + 1}: {individuo}")
     
